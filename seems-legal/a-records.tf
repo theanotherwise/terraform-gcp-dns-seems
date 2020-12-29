398eb2495492fd2a7f17a75a8c4c2eb872c3d9f4
Your Name <you@example.com>
2020-12-30 00:35:50 +0100

    #

--- seems-legal/a-records.tf
+++ seems-legal/a-records.tf
@@ -1,5 +1,44 @@
-resource "google_dns_record_set" "aaaa-seems-legal" {
+resource "google_dns_record_set" "a-raszyn-seems-legal" {
   name = "raszyn.seems.legal."
+  managed_zone = google_dns_managed_zone.public-seems-legal.name
+  type = "A"
+  ttl = 300
+
+  rrdatas = [
+    "45.137.227.44"]
+
+  depends_on = [
+    google_dns_managed_zone.public-seems-legal]
+}
+
+resource "google_dns_record_set" "a-www-raszyn-seems-legal" {
+  name = "www.raszyn.seems.legal."
+  managed_zone = google_dns_managed_zone.public-seems-legal.name
+  type = "A"
+  ttl = 300
+
+  rrdatas = [
+    "45.137.227.44"]
+
+  depends_on = [
+    google_dns_managed_zone.public-seems-legal]
+}
+
+resource "google_dns_record_set" "a-ip-raszyn-seems-legal" {
+  name = "ip.raszyn.seems.legal."
+  managed_zone = google_dns_managed_zone.public-seems-legal.name
+  type = "A"
+  ttl = 300
+
+  rrdatas = [
+    "45.137.227.44"]
+
+  depends_on = [
+    google_dns_managed_zone.public-seems-legal]
+}
+
+resource "google_dns_record_set" "a-www-ip-raszyn-seems-legal" {
+  name = "www.ip.raszyn.seems.legal."
   managed_zone = google_dns_managed_zone.public-seems-legal.name
   type = "A"
   ttl = 300
